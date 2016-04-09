e8dbc655472dca18c776a1b4f04b3aa0c6097a7d
ilovetux <me@ilovetux.com>
2016-04-08 21:02:53 -0400

    Fix import error

--- mast/testsuite/unit/__main__.py
+++
@@ -1,7 +0,0 @@
-import os
-import unittest
-
-here = os.path.dirname(__file__)
-suite = unittest.defaultTestLoader.discover(start_dir=here,
-                                            top_level_dir=here)
-unittest.TextTestRunner(verbosity=2).run(suite)
